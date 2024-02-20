f8e446064890be331fae54be0acaa74061594959
VidaGerg98 <48093658+VidaGerg98@users.noreply.github.com>
2024-02-20 15:44:31 +0100

    Add files via upload
    Init

---
+++ SocialMediaLinks/SocialMediaLinks/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace SocialMediaLinks.Controllers {
+	public class HomeController : Controller {
+
+		private readonly SocialMediaLinksOptions _socialMediaLinksOptions;
+
+		public HomeController(IOptions<SocialMediaLinksOptions> socialMediaOptions) {
+			_socialMediaLinksOptions = socialMediaOptions.Value;
+		}
+
+		[Route("/")]
+		public IActionResult Index() {
+			ViewBag.Facebook = _socialMediaLinksOptions.Facebook;
+			ViewBag.Instagram = _socialMediaLinksOptions.Instagram;
+			ViewBag.Youtube = _socialMediaLinksOptions.Youtube;
+			ViewBag.Twitter = _socialMediaLinksOptions.Twitter;
+			return View();
+		}
+	}
+}
